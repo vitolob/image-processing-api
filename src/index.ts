f0279f6890bfb8af31de5bcf38c588fc7e6bfcdc
Victorino Machava <victorino.machava@vm.co.mz>
2022-12-27 22:29:31 +0200

    chore: add and integrate eslint and prettier for linting and formatting

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
 
 const app: Application = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "Hello world!"
-    });
+  res.json({
+    message: "Hello world!",
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
