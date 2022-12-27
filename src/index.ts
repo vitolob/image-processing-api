5cb796569c3ef31ed8c01e23c01a9abdf2ac5b27
Victorino Machava <victorino.machava@vm.co.mz>
2022-12-27 21:55:32 +0200

    chore: install, configure typescript and setup basic server with express

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Application} from "express";
+
+const app: Application = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello world!"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
