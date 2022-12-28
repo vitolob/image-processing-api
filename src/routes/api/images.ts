dc98b57cdc71a72ca07e111d2aad94941fd22352
Victorino Machava <victorino.machava@vm.co.mz>
2022-12-28 22:11:39 +0200

    feat: add resizing functionality to images endpoint

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,18 +1,22 @@
 import { Router } from "express";
 import fs from "fs";
+import Sharp from "sharp";
 
 const router = Router();
 
 router.get("/", (req, res) => {
   const { filename, width, height } = req.query;
 
-  fs.readFile(`./assets/${filename}`, (err, data) => {
-    if (err) {
+  Sharp(`./assets/${filename}`)
+    .resize(Number(width), Number(height))
+    .toBuffer()
+    .then((data) => {
+      res.set("Content-Type", "image/jpeg");
+      res.send(data);
+    })
+    .catch((err) => {
       res.status(500).send(err);
-    } else {
-      res.status(200).type("image/jpeg").send(data);
-    }
-  });
+    });
 });
 
 export default router;
