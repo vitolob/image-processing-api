831756f65a5cb63421ee15fda171d6d04be9d75e
Victorino Machava <victorino.machava@vm.co.mz>
2022-12-28 19:12:47 +0200

    refactor: move endpoint into routes module

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import express, { Application } from "express";
+import routes from "./routes";
 
 const app: Application = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello world!",
-  });
-});
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
