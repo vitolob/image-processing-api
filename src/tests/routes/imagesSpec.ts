94bec13dd508959e51fef25919549258411aff51
Victorino Machava <victorino.machava@vm.co.mz>
2022-12-30 15:48:04 +0200

    test(images): improve test to clean up generated cache files

--- src/tests/routes/imagesSpec.ts
+++ src/tests/routes/imagesSpec.ts
@@ -2,14 +2,27 @@ import app from "../../index";
 import supertest from "supertest";
 import { existsSync } from "fs";
 import sharp from "sharp";
+import * as fs from "fs";
 
 const request = supertest(app);
 
 describe("GET /api/images endpoint", () => {
+  beforeEach(() => {
+    // Create the 'assets/cache' directory if it does not exist
+    if (!fs.existsSync("assets/cache")) {
+      fs.mkdirSync("assets/cache");
+    }
+  });
+
+  afterEach(() => {
+    // Delete the cached image after each test case
+    fs.unlinkSync("assets/cache/100x100-test-image.jpg");
+  });
+
   it("should return a 200 status code and an image file", async () => {
     const response = await request
       .get("/api/images")
-      .query({ filename: "fjord.jpg", width: "200", height: "200" });
+      .query({ filename: "test-image.jpg", width: "100", height: "100" });
 
     expect(response.status).toBe(200);
     expect(response.type).toBe("image/jpeg");
@@ -18,23 +31,23 @@ describe("GET /api/images endpoint", () => {
   it("should resize the image", async () => {
     const response = await request
       .get("/api/images")
-      .query({ filename: "fjord.jpg", width: "300", height: "300" });
+      .query({ filename: "test-image.jpg", width: "100", height: "100" });
     expect(response.status).toBe(200);
 
     // Get resized image and compare its dimensions to the expected dimensions
     const image = sharp(response.body);
     const metadata = await image.metadata();
-    expect(metadata.width).toEqual(300);
-    expect(metadata.height).toEqual(300);
+    expect(metadata.width).toEqual(100);
+    expect(metadata.height).toEqual(100);
   });
 
   it("should cache the resized image", async () => {
     const res = await request
       .get("/api/images")
-      .query({ filename: "fjord.jpg", width: 100, height: 100 });
+      .query({ filename: "test-image.jpg", width: 100, height: 100 });
     expect(res.status).toBe(200);
 
-    const cachePath = `${process.cwd()}/assets/cache/100x100-fjord.jpg`;
+    const cachePath = `${process.cwd()}/assets/cache/100x100-test-image.jpg`;
     expect(existsSync(cachePath)).toBe(true);
   });
 });
