f56cebcb5fb89029346b01b588a24aca42fef9ef
Victorino Machava <victorino.machava@vm.co.mz>
2022-12-30 20:18:57 +0200

    refactor(imags): use async/await in get action

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -4,7 +4,7 @@ import { resizeImage, saveImageToCache } from "../../utils/image-processing";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response): void => {
+router.get("/", async (req: Request, res: Response): Promise<void> => {
   const { filename, width, height } = req.query;
 
   // Check if resized image exists
@@ -14,27 +14,33 @@ router.get("/", (req: Request, res: Response): void => {
     return;
   }
 
-  // Resize and save image in cache
-  resizeImage(filename as string, Number(width), Number(height))
-    .then((data) => {
-      return saveImageToCache(
-        data,
-        filename as string,
-        Number(width),
-        Number(height)
-      );
-    })
-    .then(() => {
-      res.set("Content-Type", "image/jpeg");
-      res.sendFile(cachePath);
-    })
-    .catch((err) => {
+  try {
+    // Resize and save image in cache
+    const data = await resizeImage(
+      filename as string,
+      Number(width),
+      Number(height)
+    );
+    await saveImageToCache(
+      data,
+      filename as string,
+      Number(width),
+      Number(height)
+    );
+
+    res.set("Content-Type", "image/jpeg");
+    res.sendFile(cachePath);
+  } catch (err) {
+    if (err instanceof Error) {
       if (err.message.includes("file is missing")) {
         res.status(404).send({ error: err.message });
       } else {
         res.status(500).send({ error: err.message });
       }
-    });
+    } else {
+      throw err;
+    }
+  }
 });
 
 export default router;
